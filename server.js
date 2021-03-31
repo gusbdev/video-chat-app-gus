c1cb2ae9a6081146ecd09d0d24af245169ab0792
gusbdev <gusbdev@gmail.com>
2021-03-31 19:40:25 -0300

    Server file config

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server = app.listen(process.env.PORT || 5000, function () {
+app.listen(process.env.PORT || 5000, function () {
   var port = server.address().port;
   console.log("Express is working on port " + port);
 });
